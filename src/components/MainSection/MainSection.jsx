0e927b95d7c5b6ecdf1c85b53a4d17ebb0138e7b
Anubhav <anubhavpant2000@gmail.com>
2024-03-12 11:52:34 +0530

    updated commit

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -6,9 +6,9 @@ const MainSection = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi,I'm Annnnnnubhav</h1>
+        <h1 className={styles.title}>Hi,I'm Anubhav</h1>
         <p className={styles.description}>I'm a Software Engineer</p>
-        <button  onClick={()=>{window.open("../../../resume/AnubhavResume.pdf")}}  className={styles.contactBtn}>Download Resume</button>
+        <button  onClick={()=>{window.open(getImageUrl("AnubhavResume.pdf"))}}  className={styles.contactBtn}>Download Resume</button>
       </div>
       <img src={getImageUrl("main/mainImage.png")}
       alt="abc"
